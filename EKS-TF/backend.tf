c47a01148a7d2699a965dca5c2530f1284a2cd69
haddaprashant6-del <haddaprashant6@gmail.com>
2026-04-12 00:09:41 +0530

    Update backend.tf

--- EKS-TF/backend.tf
+++ EKS-TF/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "test32143"  # Ensure this is your actual S3 bucket name
     key            = "eks/terraform.tfstate"  # Standardized directory naming
-    region         = "ap-south-1"  # Changed to match provider region
+    region         = "us-east-1"  # Changed to match provider region
     encrypt        = true  # Ensures state file is encrypted
     dynamodb_table = "terraform-lock"  # Enables state locking to prevent conflicts
   }
