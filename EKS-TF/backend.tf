bcefbd3f1828458b530a2ac313e845745a745421
haddaprashant6-del <haddaprashant6@gmail.com>
2026-04-11 23:57:08 +0530

    Update backend.tf

--- EKS-TF/backend.tf
+++ EKS-TF/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "mario12bucket"  # Ensure this is your actual S3 bucket
+    bucket         = "test32143"  # Ensure this is your actual S3 bucket
     key            = "eks/terraform.tfstate"  # Standardized directory naming
     region         = "ap-south-1"  # Changed to match provider region
     encrypt        = true  # Ensures state file is encrypted
