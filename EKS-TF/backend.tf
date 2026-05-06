5eb301e2e6ef9aab44e57a193c070832ce0221f5
haddaprashant6-del <haddaprashant6@gmail.com>
2026-05-07 00:18:57 +0530

    test

--- EKS-TF/backend.tf
+++ EKS-TF/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "test321431 "  # Ensure this is your actual S3 bucket name
+    bucket         = "test321431"  # Ensure this is your actual S3 bucket name
     key            = "eks/terraform.tfstate"  # Standardized directory naming
     region         = "us-east-1"  # Changed to match provider region
     encrypt        = true  # Ensures state file is encrypted
